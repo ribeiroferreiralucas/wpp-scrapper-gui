25b76c9cbf673dd0d95e31142b72f9f26207f646
Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
2021-04-19 19:34:26 -0300

    Squashed commit of the following:
    commit 2049ce4a53302078b2babc79a5d2f264ade74317
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 19:21:47 2021 -0300
    
        Update go.yml
    
    commit 63b9544c014ce57e02e5e75d4db7c426605d2356
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 19:11:40 2021 -0300
    
        Update go.yml
    
    commit b5ee20dcc6e93948a9cec3a921c2318f76dcc5f9
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 16:04:02 2021 -0300
    
        Update go.yml
    
    commit 7133105816ebff3c177005d18a3134fda0e4ce72
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 15:52:01 2021 -0300
    
        Update go.yml
    
    commit 3d3b2b296094befeba51414417aa6d3c1edd9427
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 15:45:51 2021 -0300
    
        Update go.yml
    
    commit 3ad1423169c5d8305adc3c4db22c67b007e4fe12
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 15:37:26 2021 -0300
    
        Update go.yml
    
    commit 861efa3639900ffdeee68103d0787d8c58c0eaa6
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 15:24:39 2021 -0300
    
        Update go.yml
    
    commit ba643e16272485e97211901ea65042e32e004733
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 15:20:20 2021 -0300
    
        Update go.yml
    
    commit 445c1a6b5604904253786c9e26b7f1ae8ce7b49b
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 15:05:01 2021 -0300
    
        Update go.yml
    
    commit 948495b9f54ff271e132d3d00c1dd48406e9b117
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 14:44:18 2021 -0300
    
        Update go.yml
    
    commit 0dc0269a52c21878ee29ffde6b2fe34c8b8860c8
    Merge: 789dc08 d3aa9e1
    Author: Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 13:45:32 2021 -0300
    
        Merge branch 'master' into config-ci-builds
    
    commit d3aa9e1e4565b6220932d189543edeb3dfc2d871
    Author: Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 13:44:40 2021 -0300
    
        downgrade golang version requeriment
    
    commit 789dc08f133a045eb4abbe9b1d74afb45f840513
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 13:30:35 2021 -0300
    
        Update go.yml
    
    commit 47eccd2db7f6e7454a3d9cdc0a0bfa757c20beaa
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 13:11:21 2021 -0300
    
        Update go.yml
    
    commit 347153d7e53e150bffc30c1f13a9464f616054d8
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 13:06:04 2021 -0300
    
        Update go.yml
    
    commit 1ac4dab68b4c35c4518781d994d4eae082eed7a4
    Author: Lucas Ribeiro <ribeiroferreiralucas@gmail.com>
    Date:   Mon Apr 19 13:03:17 2021 -0300
    
        Create go.yml
    
    commit 30b6381b7d339120c5a4254e6168a370d43a3deb
    Author: Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
    Date:   Sun Apr 18 22:04:21 2021 -0300
    
        Implementação finalizada
    
    commit 79bfb071d7c4f731ef2305acb9c815b7800cb740
    Author: Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
    Date:   Sun Apr 18 15:16:19 2021 -0300
    
        Excluindo arquivos desnecessários
    
    commit bb9a079cdc1a67338d174881cec54c72dfa841fa
    Author: wilsonoliveira <wilson_oliveira07@hotmail.com>
    Date:   Fri Apr 16 02:03:27 2021 -0300
    
        lista com chats
    
    commit 0af68131af32001367111998c1a0d70fedf73d2d
    Author: wilsonoliveira <wilson_oliveira07@hotmail.com>
    Date:   Sun Feb 28 20:28:03 2021 -0300
    
        Add files via upload
    
    commit 0ad75970cc25cbf59c440c0aa680872aafc9f248
    Author: wilsonoliveira <wilson_oliveira07@hotmail.com>
    Date:   Sun Feb 28 20:27:05 2021 -0300
    
        Add files via upload
    
    commit 386f218490454ff2a110da05df3b54bb4fac1337
    Author: wilsonoliveira <wilson_oliveira07@hotmail.com>
    Date:   Sun Feb 28 20:26:39 2021 -0300
    
        Add files via upload

---
+++ go.mod
@@ -0,0 +1,11 @@
+module wppscrappergui
+
+go 1.15
+
+require (
+	fyne.io/fyne/v2 v2.0.2
+	github.com/boombuler/barcode v1.0.1
+	github.com/ribeiroferreiralucas/wpp-scrapper v0.0.0-20210419000047-1e4b76aedcda
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
+	wppscrapper "github.com/ribeiroferreiralucas/wpp-scrapper"
+	"github.com/ribeiroferreiralucas/wpp-scrapper/wppscrapperimp"
+)
+
+var application = app.NewWithID("br.ufrrj.wppscrappergui")
+var wdw = application.NewWindow("WppScrapper GUI")
+
+var wppScrapper = wppscrapperimp.InitializeConnection().(wppscrapper.IWppScrapper)
+
+func main() {
+	application.SetIcon(theme.FyneLogo())
+
+	showInitialLoadingView()
+
+	wdw.Resize(fyne.NewSize(640, 460))
+	go initializeWppScrapper()
+	wdw.ShowAndRun()
+}
+
+func showInitialLoadingView() fyne.CanvasObject {
+
+	title := canvas.NewText("Loaging", theme.TextColor())
+	cont := container.New(layout.NewCenterLayout(), title)
+
+	wdw.SetContent(cont)
+	return cont
+}
+
+func showQrCodeView(qrCode string) fyne.CanvasObject {
+
+	title := canvas.NewText("Scan the QRCode using your WhatsApp app", theme.ForegroundColor())
+	qrImage := getQrCodeImage(qrCode)
+	qrImage.FillMode = canvas.ImageFillContain
+	qrImage.Refresh()
+	cont := container.NewBorder(title, nil, nil, nil, qrImage)
+
+	wdw.SetContent(cont)
+	return cont
+}
+
+func showMainView() {
+	mainView := &MainView{}
+	// mainView.WppScrapper = wppScrapper
+	// mainView.Wdw = wdw
+
+	mainView.Show()
+}
+
+func initializeWppScrapper() {
+
+	qrCode := make(chan string)
+	go func() {
+		showQrCodeView(<-qrCode)
+	}()
+
+	_, err := wppScrapper.ReAuth(qrCode, application.UniqueID())
+	if err != nil {
+		//TODO: tratar erro
+		fmt.Println("Error trying to auth", err)
+
+		initializeWppScrapper()
+		return
+	}
+
+	showInitialLoadingView()
+
+	if !wppScrapper.Initialized() {
+		<-wppScrapper.WaitInitialization()
+	}
+
+	showMainView()
+}
+
+func getQrCodeImage(qrCodeValue string) *canvas.Image {
+	qrCode, _ := qr.Encode(qrCodeValue, qr.M, qr.Auto)
+	qrCode, _ = barcode.Scale(qrCode, 512, 512)
+
+	file, _ := os.Create(os.TempDir() + "/wppscrappergui_qrcode.png")
+	defer file.Close()
+	fmt.Println(file.Name())
+	// encode the barcode as png
+	png.Encode(file, qrCode)
+
+	image := &canvas.Image{
+		File: file.Name(),
+	}
+
+	return image
+}
